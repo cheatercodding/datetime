454485fd637925a9c08a076c821441cfa762534f
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-09 14:48:39 +0300

    zaman dilimi oluşturulması

--- bu_da_şuanki_saatlere_göre_olan_hali.py
+++ bu_da_şuanki_saatlere_göre_olan_hali.py
@@ -7,3 +7,10 @@ utc_timezone_str = 'UTC'
 turkey_timezone_str = 'Europe/Istanbul'
 new_york_zaman_dilimi_str = 'America/New_York'
 
+
+# Zaman dilimlerini oluşturulması
+utc_timezone = pytz.timezone(utc_timezone_str)
+turkey_timezone = pytz.timezone(turkey_timezone_str)
+new_york_zaman_dilimi = pytz.timezone(new_york_zaman_dilimi_str)
+
+
