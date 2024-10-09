8744e5c40ab3105d8b6735a6596a00292c83ccd6
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-09 15:14:09 +0300

    şu anki zamanı her dilimde yazdırılıması

--- yeniproje_tüm_zaman.py
+++ yeniproje_tüm_zaman.py
@@ -4,3 +4,10 @@ import pytz
 # Tüm zaman dilimleri
 tüm_zaman_dilimleri = pytz.all_timezones
 
+# Şu anki zamanı her bir zaman diliminde yazdırılması
+for zaman_dilimi_str in tüm_zaman_dilimleri:
+    zaman_dilimi = pytz.timezone(zaman_dilimi_str)
+    
+    
+
+
