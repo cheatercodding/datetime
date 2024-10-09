0bc4cb2571d82e6e3bf7508ec0c3f6c6ba35f747
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-09 14:49:36 +0300

    şuanki zamanı utc zaman diliminde ayarlanması

--- bu_da_şuanki_saatlere_göre_olan_hali.py
+++ bu_da_şuanki_saatlere_göre_olan_hali.py
@@ -14,3 +14,10 @@ turkey_timezone = pytz.timezone(turkey_timezone_str)
 new_york_zaman_dilimi = pytz.timezone(new_york_zaman_dilimi_str)
 
 
+# Şu anki zamanı al ve UTC zaman diliminde ayarlanması
+current_time = datetime.datetime.now()
+current_time_utc = utc_timezone.localize(current_time)
+
+
+
+
