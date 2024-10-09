e9b024f9894cfbcafcbcfe0c76e353cfb34a024f
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-09 14:30:59 +0300

    domain süresi biiş tarihini çevirme

--- newproje2_semih abinin_yaptığı.py
+++ newproje2_semih abinin_yaptığı.py
@@ -10,3 +10,6 @@ domain_expire_date_str = "2024-10-04 10:00"
 utc_timezone = pytz.timezone(utc_timezone_str)
 turkey_timezone = pytz.timezone(turkey_timezone_str)
 
+# Domain süresi bitiş tarihini string'den datetime nesnesine çevir
+domain_expire_date = datetime.datetime.strptime(domain_expire_date_str, '%Y-%m-%d %H:%M')
+
