da9b3dbb4be3a8ba451a2e7ae0005559b49d256e
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-09 14:48:09 +0300

    zaman dilimi tanımlanması

--- bu_da_şuanki_saatlere_göre_olan_hali.py
+++ bu_da_şuanki_saatlere_göre_olan_hali.py
@@ -1,3 +1,9 @@
 import datetime
 import pytz
 
+
+# Zaman dilimi tanımlamalanması
+utc_timezone_str = 'UTC'
+turkey_timezone_str = 'Europe/Istanbul'
+new_york_zaman_dilimi_str = 'America/New_York'
+
