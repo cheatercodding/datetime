7a1842d69c4cb655cabecd8c761ce14128cd4be6
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-09 15:14:49 +0300

    zaman dilimnde şuanki zamanı alması

--- yeniproje_tüm_zaman.py
+++ yeniproje_tüm_zaman.py
@@ -7,6 +7,11 @@ tüm_zaman_dilimleri = pytz.all_timezones
 # Şu anki zamanı her bir zaman diliminde yazdırılması
 for zaman_dilimi_str in tüm_zaman_dilimleri:
     zaman_dilimi = pytz.timezone(zaman_dilimi_str)
+
+    # Zaman diliminde şu anki zamanı alınması
+    current_time = datetime.datetime.now(zaman_dilimi)
+    
+
     
     
 
